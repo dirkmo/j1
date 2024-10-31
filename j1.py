0b75cf094b208a0e770c203f0df281c571bf6d2e
Dirk Möller <moeller.dirk80@gmail.com>
2024-10-31 21:07:43 +0100

    dev

--- j1.py
+++ j1.py
@@ -10,6 +10,7 @@ from amaranth.back import verilog
 
 class J1(wiring.Component):
     WIDTH = 16
+    DEPTH = 4 # actual depth is 2**DEPTH
 
     def __init__(self):
         self.io_rd = Signal()
@@ -17,6 +18,7 @@ class J1(wiring.Component):
         self.mem_addr = Signal(16)
         self.mem_wr = Signal()
         self.dout = Signal(J1.WIDTH)
+        self.mem_din = Signal(J1.WIDTH)
         self.io_din = Signal(J1.WIDTH)
         self.code_addr = Signal(13)
         self.insn = Signal(16)
@@ -37,15 +39,13 @@ class J1(wiring.Component):
         # program counter
         pc = Signal(13)
         pcN = Signal(13)
+        rsp = Signal(J1.WIDTH)
+        rspN = Signal(J1.WIDTH)
+        rstkW = Signal() # return stack write
+        rstkD = Signal(J1.WIDTH) # return stack write value
+        reboot = Signal(1, init=1)
         pc_plus_1 = Signal(13)
         m.d.comb += pc_plus_1.eq(pc + 1)
-
-        # return stack write
-        rstkW = Signal()
-        # return stack write value
-        rstkD = Signal(J1.WIDTH)
-
-        reboot = Signal(1, init=1)
 
         m.d.comb += [
             self.mem_addr.eq(st0[0:16]),
@@ -60,137 +60,122 @@ class J1(wiring.Component):
         # stack2 #(.DEPTH(15)) dstack(.clk(clk), .rd(st1),  .we(dstkW), .wd(st0),   .delta(dspI));
         # stack2 #(.DEPTH(17)) rstack(.clk(clk), .rd(rst0), .we(rstkW), .wd(rstkD), .delta(rspI));
 
-        minus = Signal(17)
-        m.d.comb += minus.eq(Cat(~st0, C(1,1)) + st1 + 1)
-        self.signedless = Signal()
-        with m.If(st0[15] ^ st1[15]):
-            m.d.comb += self.signedless.eq(st1[15])
-        with m.Else():
-            m.d.comb += self.signedless.eq(minus[16])
+        # TODO:
+        dstack = Array(Signal(J1.WIDTH) for _ in range(2**J1.DEPTH))
+        rstack = Array(Signal(J1.WIDTH) for _ in range(2**J1.DEPTH))
 
-        mze = Signal() # minus==0 sign-extended
-        me = Signal() # minus sign-extended
+        st1_signed = Signal(signed(J1.WIDTH))
+        st0_signed = Signal(signed(J1.WIDTH))
+        m.d.comb += [ st1_signed.eq(st1), st0_signed.eq(st0) ]
 
-        with m.Switch(Cat(self.insn[8:16], pc[12])):
-            with m.Case('1 --- -----'):
-                m.d.comb += st0N.eq(self.insn)
-            with m.Case('0 1-- -----'): # literal
+        with m.Switch(self.insn[8:16]):
+            with m.Case('1-- -----'): # literal
                 m.d.comb += st0N.eq(Cat(self.insn[0:15], C(0, J1.WIDTH-15)))
-            with m.Case('0 000 -----'): # jump
+            with m.Case('000 -----'): # jump
                 m.d.comb += st0N.eq(st0)
-            with m.Case('0 010 -----'): # call
+            with m.Case('010 -----'): # call
                 m.d.comb += st0N.eq(st0)
-            with m.Case('0 001 -----'): # conditional jump
+            with m.Case('001 -----'): # conditional jump
                 m.d.comb += st0N.eq(st1)
-            with m.Case('0 011 -0000'): # ALU operations...
+            with m.Case('011 -0000'): # ALU operations...
                 m.d.comb += st0N.eq(st0)
-            with m.Case('0 011 -0001'):
+            with m.Case('011 -0001'):
                 m.d.comb += st0N.eq(st1)
-            with m.Case('0 011 -0010'):
+            with m.Case('011 -0010'):
                 m.d.comb += st0N.eq(st0 + st1)
-            with m.Case('0 011 -0011'):
+            with m.Case('011 -0011'):
                 m.d.comb += st0N.eq(st0 & st1)
-            with m.Case('0 011 -0100'):
+            with m.Case('011 -0100'):
                 m.d.comb += st0N.eq(st0 | st1)
-            with m.Case('0 011 -0101'):
+            with m.Case('011 -0101'):
                 m.d.comb += st0N.eq(st0 ^ st1)
-            with m.Case('0 011 -0110'):
+            with m.Case('011 -0110'):
                 m.d.comb += st0N.eq(~st0)
-
-            with m.Case('0 011 -0111'): #  =
-                m.d.comb += [
-                    mze.eq(minus==0),
-                    st0N.eq(mze.replicate(J1.WIDTH))
-                ]
-            with m.Case('0 011 -1000'): #  <
-                m.d.comb += st0N.eq(self.signedless.replicate(J1.WIDTH))
-
-            with m.Case('0 011 -1001'):
+            with m.Case('011 -0111'): #  ==
+                m.d.comb += st0N.eq(st1 == st0)
+            with m.Case('011 -1000'): #  <
+                m.d.comb += st0N.eq(st1_signed < st0_signed)
+            with m.Case('011 -1001'): # st1 >> 1
                 m.d.comb += st0N.eq(Cat(st0[1:J1.WIDTH], st0[J1.WIDTH-1]))
-            with m.Case('0 011 -1010'):
+            with m.Case('011 -1010'): # st1 << 1
                 m.d.comb += st0N.eq(Cat(C(0,1), st0[0:J1.WIDTH-1]))
-            with m.Case('0 011 -1011'):
+            with m.Case('011 -1011'):
                 m.d.comb += st0N.eq(rst0)
-            with m.Case('0 011 -1100'):
-                m.d.comb += st0N.eq(minus[0:16])
-            with m.Case('0 011 -1101'):
+            with m.Case('011 -1100'):
+                m.d.comb += st0N.eq(self.mem_din)
+            with m.Case('011 -1101'):
                 m.d.comb += st0N.eq(self.io_din)
-            with m.Case('0 011 -1110'):
-                m.d.comb += st0N.eq(Cat(dsp, C(0, J1.WIDTH-4)))
-            with m.Case('0 011 -1111'): # u<
-                m.d.comb += [
-                    me.eq(minus[16]),
-                    st0N.eq(me.replicate(J1.WIDTH))
-                ]
+            with m.Case('011 -1110'):
+                m.d.comb += st0N.eq(Cat(dsp, rsp, C(0, J1.WIDTH-8)))
+            with m.Case('011 -1111'): # u<
+                m.d.comb += st0N.eq(st1 < st0)
             with m.Default():
                 # st0N = {`WIDTH{1'bx}};
                 m.d.comb += st0N.eq(0)
 
 
         func_T_N = Signal()
-        m.d.comb += func_T_N.eq(self.insn[4:7] == 1)
         func_T_R = Signal()
-        m.d.comb += func_T_R.eq(self.insn[4:7] == 2)
         func_write = Signal()
-        m.d.comb += func_write.eq(self.insn[4:7] == 3)
         func_iow = Signal()
-        m.d.comb += func_iow.eq(self.insn[4:7] == 4)
         func_ior = Signal()
-        m.d.comb += func_ior.eq(self.insn[4:7] == 5)
+        m.d.comb += [
+            func_T_N.eq(self.insn[4:7] == 1),
+            func_T_R.eq(self.insn[4:7] == 2),
+            func_write.eq(self.insn[4:7] == 3),
+            func_iow.eq(self.insn[4:7] == 4),
+            func_ior.eq(self.insn[4:7] == 5)
+        ]
 
         is_alu = Signal()
-        m.d.comb += is_alu.eq(~pc[12] & (self.insn[13:16] == 0b011))
         mem_wr = Signal()
-        m.d.comb += mem_wr.eq(~reboot & is_alu & func_write)
-        m.d.comb += self.dout.eq(st1)
-        m.d.comb += self.io_wr.eq(~reboot & is_alu & func_iow)
-        m.d.comb += self.io_rd.eq(~reboot & is_alu & func_ior)
+        m.d.comb += [
+            is_alu.eq(self.insn[13:16] == 0b011),
+            mem_wr.eq(~reboot & is_alu & func_write),
+            self.dout.eq(st1),
+            self.io_wr.eq(~reboot & is_alu & func_iow),
+            self.io_rd.eq(~reboot & is_alu & func_ior)
+        ]
 
-        with m.If(self.insn[13] == 0):
-            m.d.comb += rstkD.eq(Cat(C(0,1), pc_plus_1, C(0, J1.WIDTH-14)))
-        with m.Else():
-            m.d.comb += rstkD.eq(st0)
+        m.d.comb += rstkD.eq(Mux(
+                self.insn[13],
+                st0,
+                Cat(C(0,1), pc_plus_1, C(0, J1.WIDTH-14))
+            ))
 
-        dspI = Signal(2)
-        rspI = Signal(2)
+        dspI = Signal(J1.DEPTH)
+        rspI = Signal(J1.DEPTH)
 
-        with m.Switch(Cat(self.insn[13:16], pc[12])):
-            with m.Case('1 ---', '0 1--'):
-                m.d.comb += [ dstkW.eq(1), dspI.eq(0b01) ]
-            with m.Case('0 001'):
-                m.d.comb += [ dstkW.eq(0), dspI.eq(0b11) ]
-            with m.Case('0 011'):
-                m.d.comb += [ dstkW.eq(func_T_N), dspI.eq(self.insn[0:2]) ]
-            with m.Default():
-                m.d.comb += [ dstkW.eq(0), dspI.eq(0) ]
+        with m.Switch(self.insn[13:16]):
+            with m.Case('1--'): m.d.comb += [ dstkW.eq(1), dspI.eq(0b0001) ]
+            with m.Case('001'): m.d.comb += [ dstkW.eq(0), dspI.eq(0b1111) ]
+            with m.Case('011'): m.d.comb += [ dstkW.eq(func_T_N), dspI.eq(Cat(self.insn[0:2], self.insn[1], self.insn[1])) ]
+            with m.Default():   m.d.comb += [ dstkW.eq(0), dspI.eq(0) ]
 
-        m.d.comb += dspN.eq(dsp + Cat(dspI, dspI[1], dspI[1]))
+        m.d.comb += dspN.eq(dsp + dspI)
 
-        with m.Switch(Cat(self.insn[13:16], pc[12])):
-            with m.Case('1 ---'):
-                m.d.comb += [ rstkW.eq(0), rspI.eq(0b11) ]
-            with m.Case('0 010'):
-                m.d.comb += [ rstkW.eq(1), rspI.eq(0b01) ]
-            with m.Case('0 011'):
-                m.d.comb += [ rstkW.eq(func_T_R), rspI.eq(self.insn[2:4]) ]
-            with m.Default():
-                m.d.comb += [ rstkW.eq(0), rspI.eq(0) ]
+        with m.Switch(self.insn[13:16]):
+            with m.Case('010'): m.d.comb += [ rstkW.eq(1), rspI.eq(0b0001) ]
+            with m.Case('011'): m.d.comb += [ rstkW.eq(func_T_R), rspI.eq(Cat(self.insn[2:4],self.insn[3], self.insn[3])) ]
+            with m.Default():   m.d.comb += [ rstkW.eq(0), rspI.eq(0) ]
 
-        with m.Switch(Cat(st0.any(), self.insn[7], self.insn[13:16], pc[12], reboot)):
+        m.d.comb += rspN.eq(rsp + rspI)
 
-            with m.Case("1 0 --- - -"):
-                m.d.comb += pcN.eq(0)
-            with m.Case("0 0 000 - -", "0 0 010 - -", "0 0 001 - 0"):
-                m.d.comb += pcN.eq(self.insn[0:13])
-            with m.Case("0 1 --- - -", "0 0 011 1 -"):
-                m.d.comb += pcN.eq(rst0[1:14])
-            with m.Default():
-                m.d.comb += pcN.eq(pc_plus_1)
+        with m.Switch(Cat(st0.any(), self.insn[7], self.insn[13:16], reboot)):
+
+            with m.Case("1 --- - -"): m.d.comb += pcN.eq(0)
+            with m.Case("0 000 - -",
+                        "0 010 - -",
+                        "0 001 - 0"): m.d.comb += pcN.eq(self.insn[0:13])
+            with m.Case("0 --- - -",
+                        "0 011 1 -"): m.d.comb += pcN.eq(rst0[1:14])
+            with m.Default():         m.d.comb += pcN.eq(pc_plus_1)
 
         m.d.sync += [
             pc.eq(pc_plus_1),
             dsp.eq(dspN),
-            st0.eq(st0N)
+            st0.eq(st0N),
+            rsp.eq(rspN)
         ]
 
         return m
